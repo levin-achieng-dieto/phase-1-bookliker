5acd7d9402b2dab1e2d662a7238a8ba1c2adcfcc
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-09-05 18:21:50 +0300

    assignment

--- js/index.js
+++ js/index.js
@@ -1,1 +1,57 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+    fetchingList()
+});
+
+// function renderBookList(book){
+//     const titleList = document.createElement('li')
+//     titleList.textContent = `${book.title}`
+
+//     document.querySelector('#list').append(titleList)
+// }
+
+// function fetchingList(){
+//     fetch('http://localhost:3000/books')
+//     .then(response => response.json())
+//     .then(data => data.forEach(book => renderBookList(book)))
+// }
+
+
+
+function fetchingList(){
+    fetch('http://localhost:3000/books')
+    .then(response => response.json())
+    .then(booktitle => {
+        const booklist = document.querySelector('#list')
+        booktitle.forEach( book => {
+            const title = document.createElement('li')
+            title.textContent = book.title;
+            title.addEventListener('click', BookInfo);
+            booklist.appendChild(title)
+        })
+    })
+    .catch(error => console.log(error))
+}
+
+
+function BookInfo(event){
+    const clickedBookName = event.target.textContent;
+    fetch('http://localhost:3000/books')
+    .then(response => response.json())
+    .then(books => {
+        let sameBook = books.find(element => {
+            return element.title = clickedBookName
+        });
+        updateBook(sameBook)
+    })
+    .catch(error => console.log(error))
+}
+
+function updateBook(book){
+    const bookinfo = document.querySelector('#show-panel')
+    const info = document.createElement('li')
+    info.textContent = `${book.subtitle}
+    ${book.description},
+    ${book.users}`
+
+    bookinfo.append(info)
+}
